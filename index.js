0af49f9a0313da2bca39fd608b99a893a7851066
Samir Gayatri <samirgupta19670@gmail.com>
2022-10-19 21:50:18 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+
+const app = express();
+
+
+
+
+app.use(bodyParser.json({ limit : "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit : "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://memories-app:asdfghjkl@cluster0.ncyxu17.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology:true})
+.then(()=> app.listen(PORT, ()=> console.log(`Server is running on port : ${PORT}`)))
+.catch((error)=>console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
---
+++ routes/posts.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {getPosts, createPost} from '../controllers/posts.js'
+
+
+
+
+const router = express.Router();
+
+
+router.get('/', getPosts);
+router.post('/', createPost);
+
+
+
+
+
+export default router;
